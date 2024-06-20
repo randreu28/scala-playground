37ab456a1a3a95f984aa9e8ebaf334347231f38f
Rubén Andreu <ruben@ecodeliver.tech>
2024-06-20 15:22:52 +0200

    (core): Creates a simple http request

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,25 @@
-@main def hello(): Unit =
-  println("Hello world!")
-  println(msg)
+import scala.io.Source
+import scala.util.{Try, Success, Failure}
+import java.net.{HttpURLConnection, URL}
 
-def msg = "I was compiled by Scala 3. >:) hehe"
+@main
+def main(): Unit =
+  val url = new URL("https://rickandmortyapi.com/api/character")
+  val response = fetchResponse(url)
+  response match {
+    case Success((statusCode, content)) => {
+      println(s"Status Code: $statusCode")
+      println(content)
+    }
+    case Failure(exception) => println(exception.getMessage)
+  }
+
+def fetchResponse(url: URL): Try[(Int, String)] = Try {
+  val connection = url.openConnection().asInstanceOf[HttpURLConnection]
+  connection.setRequestMethod("GET")
+  val statusCode = connection.getResponseCode
+  val content = Source.fromInputStream(connection.getInputStream).mkString
+  connection.disconnect()
+
+  (statusCode, content)
+}
