3a8f705ded3c8a797c28de33f56c13b88e4d5226
Rubén Andreu <ruben@ecodeliver.tech>
2024-06-25 10:36:34 +0200

    (core): Implements the remaining user CRUD functionality

--- src/main/scala/Persistence.scala
+++ src/main/scala/Persistence.scala
@@ -40,14 +40,7 @@ object DatabaseModule {
     val users = Iterator
       .continually((resultSet, resultSet.next()))
       .takeWhile(_._2)
-      .map { case (rs, _) =>
-        User(
-          rs.getInt("id"),
-          rs.getString("email"),
-          rs.getString("password"),
-          rs.getString("createdAt")
-        )
-      }
+      .map { case (rs, _) => User.fromResultSet(rs) }
       .toList
     resultSet.close()
     statement.close()
@@ -62,14 +55,7 @@ object DatabaseModule {
     statement.setInt(1, id)
     val resultSet = statement.executeQuery()
     val user = if (resultSet.next()) {
-      Some(
-        User(
-          resultSet.getInt("id"),
-          resultSet.getString("email"),
-          resultSet.getString("password"),
-          resultSet.getString("createdAt")
-        )
-      )
+      Some(User.fromResultSet(resultSet))
     } else {
       None
     }
@@ -79,12 +65,18 @@ object DatabaseModule {
     user
   }
 
-  // TODO: Validate email and password
   def createUser(email: String, password: String): Unit = {
     val connection = connect()
     val statement = connection.prepareStatement(
       "INSERT INTO users (email, password, createdAt) VALUES (?, ?, ?)"
     )
+    if (!User.validateEmail(email)) {
+      throw new IllegalArgumentException("Invalid email")
+    }
+    if (!User.validatePassword(password)) {
+      throw new IllegalArgumentException("Invalid password")
+    }
+
     statement.setString(1, email)
     statement.setString(2, password)
     statement.setString(3, LocalDateTime.now().toString)
@@ -93,17 +85,48 @@ object DatabaseModule {
     connection.close()
   }
 
-  // TODO: Don't let user update if password is the same as last one
+  def isUserPasswordSame(id: Int, newPassword: String): Boolean = {
+    val connection = connect()
+    val checkStatement =
+      connection.prepareStatement("SELECT password FROM users WHERE id = ?")
+    checkStatement.setInt(1, id)
+    val checkResultSet = checkStatement.executeQuery()
+    val isSame = if (checkResultSet.next()) {
+      val currentPassword = checkResultSet.getString("password")
+      currentPassword.equals(newPassword)
+    } else {
+      false
+    }
+    checkResultSet.close()
+    checkStatement.close()
+    connection.close()
+    isSame
+  }
+
   def updateUser(id: Int, email: String, password: String): Unit = {
     val connection = connect()
-    val statement = connection.prepareStatement(
+    if (isUserPasswordSame(id, password)) {
+      throw new IllegalArgumentException(
+        "New password is the same as the old one"
+      )
+    }
+
+    val updateStatement = connection.prepareStatement(
       "UPDATE users SET email = ?, password = ? WHERE id = ?"
     )
-    statement.setString(1, email)
-    statement.setString(2, password)
-    statement.setInt(3, id)
-    statement.executeUpdate()
-    statement.close()
+    if (!User.validateEmail(email)) {
+      throw new IllegalArgumentException("Invalid email")
+    }
+
+    if (!User.validatePassword(password)) {
+      throw new IllegalArgumentException("Invalid password")
+    }
+
+    updateStatement.setString(1, email)
+    updateStatement.setString(2, password)
+    updateStatement.setInt(3, id)
+    updateStatement.executeUpdate()
+    updateStatement.close()
     connection.close()
   }
 }
--- src/main/scala/models/User.scala
+++ src/main/scala/models/User.scala
@@ -2,11 +2,49 @@ package Models
 
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import spray.json.DefaultJsonProtocol
+import spray.json.DeserializationException
+import spray.json.JsString
+import spray.json.JsValue
+import spray.json.JsonFormat
 import spray.json.RootJsonFormat
 
-case class User(id: Int, email: String, password: String, createdAt: String)
+import java.text.SimpleDateFormat
+import java.util.Date
+import scala.util.matching.Regex
 
+case class User(id: Int, email: String, password: String, createdAt: Date)
 trait JsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
+  implicit val dateFormat: JsonFormat[Date] = new JsonFormat[Date] {
+    def write(date: Date) = JsString(User.dateFormat.format(date))
+    def read(json: JsValue) = json match {
+      case JsString(dateString) =>
+        User.dateFormat.parse(dateString)
+      case _ => throw new DeserializationException("Date expected")
+    }
+  }
+
   implicit val userFormat: RootJsonFormat[User] = jsonFormat4(User.apply)
   implicit val usersFormat: RootJsonFormat[List[User]] = listFormat[User]
+}
+
+object User {
+  val emailRegex =
+    """^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$""".r
+  val passwordRegex =
+    """^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*_).{6,}$""".r
+
+  val dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss")
+
+  def validateEmail(email: String): Boolean = emailRegex.matches(email)
+  def validatePassword(password: String): Boolean =
+    passwordRegex.matches(password)
+
+  def fromResultSet(rs: java.sql.ResultSet): User = {
+    User(
+      rs.getInt("id"),
+      rs.getString("email"),
+      rs.getString("password"),
+      dateFormat.parse(rs.getString("createdAt"))
+    )
+  }
 }
