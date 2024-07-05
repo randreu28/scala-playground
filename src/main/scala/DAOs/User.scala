ef9da158f7afd46898079d36157ef293d97c068c
Rubén Andreu <ruben@ecodeliver.tech>
2024-07-05 11:24:44 +0200

    (core): User DAO

--- src/main/scala/DAOs/User.scala
+++ src/main/scala/DAOs/User.scala
@@ -5,30 +5,24 @@ import doobie.implicits._
 import cats.effect.IO
 import cats.implicits._
 
-sealed trait UserBase {
-  def name: String
-  def email: String
-  def password: String
-}
-
 final case class User(
     id: Int,
     name: String,
     email: String,
     password: String
-) extends UserBase
+)
 
 final case class NewUser(
     name: String,
     email: String,
     password: String
-) extends UserBase
+)
 
 final case class UpdateUser(
     id: Int,
-    name: Option[String],
-    email: Option[String],
-    password: Option[String]
+    name: String,
+    email: String,
+    password: String
 )
 
 object UserDAO extends Database {
@@ -39,7 +33,7 @@ object UserDAO extends Database {
       .transact(xa)
   }
 
-  def selectAll: IO[List[User]] = {
+  def selectAll(): IO[List[User]] = {
     sql"SELECT * FROM users"
       .query[User]
       .to[List]
@@ -47,20 +41,22 @@ object UserDAO extends Database {
   }
 
   def insert(newUser: NewUser): IO[Int] = {
-    sql"INSERT INTO users (name, email, password) VALUES (${newUser.name}, ${newUser.email}, ${newUser.password})".update
+    sql"""
+      INSERT INTO users (name, email, password) 
+      VALUES (${newUser.name}, ${newUser.email}, ${newUser.password})
+    """.update
       .withUniqueGeneratedKeys[Int]("id")
       .transact(xa)
   }
 
   def update(updateUser: UpdateUser): IO[Int] = {
-    val query = fr"UPDATE users SET" ++
-      updateUser.name.map(name => fr"name = $name").getOrElse(fr"") ++
-      updateUser.email.map(email => fr", email = $email").getOrElse(fr"") ++
-      updateUser.password
-        .map(password => fr", password = $password")
-        .getOrElse(fr"") ++
-      fr"WHERE id = ${updateUser.id}"
-
-    query.update.run.transact(xa)
+    sql"""
+      UPDATE users 
+      SET name = ${updateUser.name}, 
+          email = ${updateUser.email}, 
+          password = ${updateUser.password} 
+      WHERE id = ${updateUser.id}
+      """.update.run
+      .transact(xa)
   }
 }
