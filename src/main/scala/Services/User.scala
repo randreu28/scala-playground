a7c03c793fc72da222e5c94ebdae349c79170d5b
Rubén Andreu <ruben@ecodeliver.tech>
2024-07-05 13:01:25 +0200

    (core): Implements the userService with the new DAO
    I'm still trying out syntax and figuring out how to do the most readable version of this

--- src/main/scala/Services/User.scala
+++ src/main/scala/Services/User.scala
@@ -1,33 +1,69 @@
 package Services
 
+import DAOs.NewUser
+import DAOs.UpdateUser
+import DAOs.User
 import DAOs.UserDAO
 import cats.effect.IO
 import org.mindrot.jbcrypt.BCrypt
-import DAOs.User
 
-//TODO: Update based on new DAO
+//TODO: Use different throwables type
 object UserService {
-  def validateEmail(email: String): Boolean = {
-    email.matches("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$")
+  def createUser(user: NewUser): IO[Int] = {
+    for {
+      _ <- validateEmail(user.email)
+      hashedPassword = hashPassword(user.password)
+      hashedUser = user.copy(password = hashedPassword)
+      affectedRows <- UserDAO.insert(hashedUser)
+    } yield affectedRows
   }
 
-  def hashPassword(password: String): String = {
+  def getUserById = UserDAO.selectById
+  def getUsers() = UserDAO.selectAll()
+
+  def updateUser(updateUser: UpdateUser): IO[Int] = {
+    for {
+      userOption <- getUserById(updateUser.id)
+      user <- validateUserExists(userOption)
+      _ <- validateEmail(updateUser.email)
+      _ <- validateNewPassword(user.password, updateUser.password)
+      hashedPassword = hashPassword(updateUser.password)
+      updatedUser = updateUser.copy(password = hashedPassword)
+      affectedRows <- UserDAO.update(updatedUser)
+    } yield affectedRows
+  }
+
+  private def hashPassword(password: String): String = {
     BCrypt.hashpw(password, BCrypt.gensalt())
   }
 
-  def createUser(user: User): IO[Either[String, Int]] = {
-    if (!validateEmail(user.email)) {
-      return IO.pure(Left("Invalid email format"))
+  private def validateNewPassword(
+      prevPassword: String,
+      newPassword: String
+  ): IO[Unit] = {
+    if (BCrypt.checkpw(newPassword, prevPassword)) {
+      return IO.raiseError(
+        new IllegalArgumentException(
+          "New password must be different from the current one"
+        )
+      )
     }
 
-    val hashedUser = user.copy(password = hashPassword(user.password))
-    UserDAO.insert(hashedUser).map(Right(_))
+    IO.unit
   }
 
-  def getUserById(id: Int): IO[Option[User]] = {
-    UserDAO.selectById(id)
+  private def validateEmail(email: String): IO[Unit] = {
+    if (!email.matches("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$")) {
+      return IO.raiseError(new IllegalArgumentException("Invalid email format"))
+    }
+
+    IO.unit
   }
 
-  def getUsers: IO[List[User]] = ???
-  def updateUser(id: Int, user: User): IO[Option[User]] = ???
+  private def validateUserExists(userOption: Option[User]): IO[User] = {
+    userOption match {
+      case Some(u) => IO.pure(u)
+      case None => IO.raiseError(new IllegalArgumentException("User not found"))
+    }
+  }
 }
