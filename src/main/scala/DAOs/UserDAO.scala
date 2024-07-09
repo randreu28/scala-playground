e762f5415767a45ff95852806c2848109994460f
Rubén Andreu <ruben@ecodeliver.tech>
2024-07-09 10:12:46 +0200

    (fix): Corrects signatures of UserDAO
    Sometimes I was asking for an Id on insert, or returning a List of users when I only needed one, etc

--- src/main/scala/DAOs/UserDAO.scala
+++ src/main/scala/DAOs/UserDAO.scala
@@ -2,30 +2,47 @@ package DAOs
 
 import io.getquill._
 
-case class User(id: Int, name: String, email: String, password: String)
+final case class User(id: Int, name: String, email: String, password: String)
+final case class NewUser(name: String, email: String, password: String)
+final case class UpdateUser(
+    id: Int,
+    name: Option[String],
+    email: Option[String],
+    password: Option[String]
+)
 
 object UserDAO extends Database {
   import ctx._
 
-  def selectAll = ctx.run(query[User])
+  def selectAll = ctx.run {
+    query[User]
+  }
 
-  def selectById(id: Int) = ctx.run(
-    quote {
+  def selectById(id: Int) = ctx
+    .run {
       query[User]
         .filter(u => u.id == lift(id))
     }
-  )
+    .map(_.headOption)
 
-  def insert(user: User) = ctx.run(
+  def insert(user: NewUser) = ctx.run {
     query[User]
-      .insertValue(lift(user))
-  )
+      .insert(
+        _.name -> lift(user.name),
+        _.password -> lift(user.password),
+        _.email -> lift(user.email)
+      )
+      .returningGenerated(_.id)
+  }
 
-  def update(user: User) = ctx
-    .run(
-      query[User]
-        .filter(u => u.id == lift(user.id))
-        .updateValue(lift(user))
-    )
+  def update(user: UpdateUser) = ctx.run {
+    dynamicQuery[User]
+      .filter(u => u.id == lift(user.id))
+      .update(
+        setOpt(u => u.name, user.name),
+        setOpt(u => u.password, user.password),
+        setOpt(u => u.email, user.email)
+      )
+  }
 
 }
