3e041e6091d34c0bcddaf83695316569b70abfa8
Rubén Andreu <ruben@ecodeliver.tech>
2024-07-09 12:02:21 +0200

    (core): Makes the UpdateUser simpler
    It would have been better to use Options on UpdateUser, but as the kata with Cats was this way too for simplicity, I'm gonna do it the same way. This will be a more 'fair' comparision

--- src/main/scala/DAOs/UserDAO.scala
+++ src/main/scala/DAOs/UserDAO.scala
@@ -6,9 +6,9 @@ final case class User(id: Int, name: String, email: String, password: String)
 final case class NewUser(name: String, email: String, password: String)
 final case class UpdateUser(
     id: Int,
-    name: Option[String],
-    email: Option[String],
-    password: Option[String]
+    name: String,
+    email: String,
+    password: String
 )
 
 object UserDAO extends Database {
@@ -36,12 +36,12 @@ object UserDAO extends Database {
   }
 
   def update(user: UpdateUser) = ctx.run {
-    dynamicQuery[User]
+    query[User]
       .filter(u => u.id == lift(user.id))
       .update(
-        setOpt(u => u.name, user.name),
-        setOpt(u => u.password, user.password),
-        setOpt(u => u.email, user.email)
+        _.name -> lift(user.name),
+        _.password -> lift(user.password),
+        _.email -> lift(user.email)
       )
   }
 
